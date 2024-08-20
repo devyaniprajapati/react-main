e63947e33648002537c5b9cc4e1f381c48c3e901
devyani prajapati <devyaniprajapati013@gmail.com>
2024-08-20 11:57:59 +0530

    navigeter

---
+++ Navigator/src/componets/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Product Catalog</h1>
+      <p>Explore a wide range of products.</p>
+    </div>
+  );
+};
+
+export default Home;
