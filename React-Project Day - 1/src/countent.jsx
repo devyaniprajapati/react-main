52dd2055298f3b1209099df124d88a3353a9f473
devyani prajapati <devyaniprajapati013@gmail.com>
2024-08-20 12:06:29 +0530

    explor

---
+++ React-Project Day - 1/src/countent.jsx
@@ -0,0 +1,13 @@
+// src/Content.js
+
+import React from 'react';
+
+function Content() {
+  return (
+    <section>
+      <p>This is the content section.</p>
+    </section>
+  );
+}
+
+export default Content;
---
+++ React-Project Day - 1/src/footer.jsx
@@ -0,0 +1,13 @@
+// src/Footer.js
+
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Footer</p>
+    </footer>
+  );
+}
+
+export default Footer;
